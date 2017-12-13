1e256d8add945b5be10d5bb1321e43f62f3d19fe
Daniel Barlow <daniel.barlow@simplybusiness.co.uk>
2017-12-13 10:18:52 +0000

    enable dual-channel recording & transcript fetching from voicebase

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'sinatra'
 require 'sinatra/json'
 require 'dotenv'
 require 'faker'
+require 'patron'
+require 'pry'
 
 # Load environment configuration
 Dotenv.load
@@ -37,7 +39,8 @@ end
 post '/voice' do
   twiml = Twilio::TwiML::VoiceResponse.new do |r|
     if params['To'] and params['To'] != ''
-      r.dial(caller_id: ENV['TWILIO_CALLER_ID']) do |d|
+      r.dial(record: "record-from-answer-dual",
+             caller_id: ENV['TWILIO_CALLER_ID']) do |d|
         # wrap the phone number or client name in the appropriate TwiML verb
         # by checking if the number given has only digits and format symbols
         if params['To'] =~ /^[\d\+\-\(\) ]+$/
@@ -53,4 +56,25 @@ post '/voice' do
 
   content_type 'text/xml'
   twiml.to_s
+end
+
+post '/transcript' do
+  json = JSON.parse(params.fetch("AddOns"))
+  url = ["results", "voicebase_transcription", "payload",
+         0, "url"].reduce(json) {|m, p| m.fetch(p) }
+  uri = URI.parse(url)
+  auth = [ENV.fetch("TWILIO_ACCOUNT_SID"),
+          ENV.fetch("TWILIO_AUTH_TOKEN")]
+  headers = {'User-Agent' => 'acdc/0.1'}
+  patron = Patron::Session.new(timeout: 20,
+                               connect_timeout: 20,
+                               username: ENV.fetch("TWILIO_ACCOUNT_SID"),
+                               password: ENV.fetch("TWILIO_AUTH_TOKEN"),
+                               base_url: uri.merge('/'),
+                               headers: headers )
+  xcript = JSON.parse(patron.get(uri.path).body)
+  IO::File.open("/tmp/transcript/#{Time.new.to_i}","w") do |f|
+    f.write(xcript.to_json)
+  end
+  "OK"
 end
